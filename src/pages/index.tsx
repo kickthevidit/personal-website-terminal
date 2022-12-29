3dd3902f75b4f1d76c0f3156101beb68ef578836
kickthevidit <vgautam2@illinois.edu>
2022-12-29 14:59:29 -0600

    added regular website button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,10 +44,10 @@ const IndexPage: React.FC<IndexPageProps> = ({ inputRef }) => {
       >
 
         <div ref={containerRef} className="overflow-y-auto h-full">
-          <div style={{ float: "left", width: "80%" }}><History history={history} />
+          <div style={{ float: "left", width: "75%" }}><History history={history} />
 
             <Input inputRef={inputRef} containerRef={containerRef} /></div>
-          <div style={{ float: "left", width: "20%", textAlign: "right" }}>
+          <div style={{ float: "left", width: "25%", textAlign: "right" }}>
             <a href="https://vidg.webflow.io" ><button
               style={{
                 alignContent: 'center',
@@ -55,14 +55,14 @@ const IndexPage: React.FC<IndexPageProps> = ({ inputRef }) => {
                 style={{
                   borderColor: theme.yellow,
                   borderWidth: 1,
-                  width: 190,
+                  width: 275,
                   borderRadius: 2,
                   margin: 5,
                   flex: 1,
                   alignContent: 'center',
-                  padding: '5px',
+                  padding: '15px',
                 }}
-              ><p style={{ color: theme.yellow, fontFamily: "sans-serif" }}>Graphical Website</p></div></button></a></div>
+              ><p style={{ color: theme.yellow, fontFamily: "sans-serif", fontSize: "18px" }}>Check out the Graphical Website!</p></div></button></a></div>
         </div>
       </div>
     </>
